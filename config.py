b900fa561d0f3ae8d947004e0c3e38f87b3c569f
ahsanMuh <ahsanmuhammadakram1996@gmail.com>
2023-05-28 02:03:32 +0500

    added add employee endpoint

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-SQL_CONN_STR = 'postgresql://postgres:stress$123@stressproject.cx561rodtayi.eu-north-1.rds.amazonaws.com:5432/stress_db'
+SQL_CONN_STR = ''
 API_ROOT_ADR = 'http://127.0.0.1:8000'
 EDF_FILE_NAME = 'input.edf'
 MODEL_DIR = './model_artifacts'
